89955241f25b4c67e2d7ce2ae0aafd06e021e60e
HiranyamayHazra101922 <hiranmoyhazra22@gmail.com>
2026-04-29 20:18:26 +0530

    Time: 156 ms (42.86%), Space: 47.8 MB (85.71%) - LeetHub

--- 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.java
+++ 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.java
@@ -5,7 +5,7 @@ class Solution {
 
     public long maximumScore(int[][] grid) {
         n = grid.length;
-        // pref[c][r] stores the sum of grid[0...r-1][c]
+       
         pref = new long[n][n + 1];
         for (int j = 0; j < n; j++) {
             for (int i = 0; i < n; i++) {
@@ -13,11 +13,10 @@ class Solution {
             }
         }
 
-        // State: [current column][height of previous column][is previous height < height before it?]
         memo = new Long[n][n + 1][2];
         
         long maxScore = 0;
-        // Try every possible height for the first column
+
         for (int h = 0; h <= n; h++) {
             maxScore = Math.max(maxScore, solve(1, h, 0));
         }
@@ -34,21 +33,12 @@ class Solution {
             long currentScore = 0;
             
             if (currH > prevH) {
-                // Column col is taller than col-1. 
-                // col-1's white cells between prevH and currH now have a black neighbor to their right.
-                // However, if we were already "increasing", those points might have been counted.
-                // If isLess == 0 (prevH was >= height at col-2), we gain new points.
                 if (isLess == 0) {
                     currentScore = pref[col - 1][currH] - pref[col - 1][prevH];
                 }
             } else if (currH < prevH) {
-                // Column col is shorter than col-1.
-                // col's white cells between currH and prevH now have a black neighbor to their left.
                 currentScore = pref[col][prevH] - pref[col][currH];
             }
-
-            // Move to next column. 
-            // New isLess is 1 if currH < prevH, else 0.
             res = Math.max(res, currentScore + solve(col + 1, currH, currH < prevH ? 1 : 0));
         }
 
